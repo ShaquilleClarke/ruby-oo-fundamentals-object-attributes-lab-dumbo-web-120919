4ccc90b6f40d38dc90dd462e96fbf82da4ad758c
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-12 20:48:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,4 @@
-class Person(name, job)
+class Person
   
   def name=(name)
     @name = name
@@ -8,6 +8,11 @@ class Person(name, job)
     @name
   end
   
+  def job
+    @job
+  end
   
-
+  def job=(job)
+    @job = job
+  end
 end
